c4f427783daaaeb567e5da5d2f416039a5f94df9
DJWL <1602951127@qq.com>
2025-01-09 20:54:43 +0800

    feat: 上传

--- src/apps/home/index.tsx
+++ src/apps/home/index.tsx
@@ -1,12 +1,31 @@
 import { useAppConfig } from '@/hooks';
-import { Button } from 'antd';
+import { uploadUrl } from '@/utils/env';
+import { Button, message, Upload, UploadProps } from 'antd';
 
 function Home() {
   const { isProd, env } = useAppConfig();
+  console.log('envv', isProd, env);
+  const props: UploadProps = {
+    name: 'file',
+    action: uploadUrl,
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
 
   return (
     <div>
       <Button type="primary">Primary Button</Button>
+      <Upload {...props}>
+        <Button>Click to Upload</Button>
+      </Upload>
     </div>
   );
 }
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,1 +1,2 @@
 export const serviceUrl = import.meta.env.PROD ? '' : 'http://localhost:3000';
+export const uploadUrl = import.meta.env.PROD ? '/api/file/upload' : 'http://localhost:3000/api/file/upload';
